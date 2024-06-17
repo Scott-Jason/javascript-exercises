42b9ce952067fc1f8e8d96b663238a97b443fb7a
Jason Scott <jcpoems@gmail.com>
2024-06-17 13:31:37 -0400

    Solve 08 problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let total = 0;
+  for( let num of nums){
+    total += num;
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let total = 1;
+  for( let num of nums){
+    total *= num;
+  }
+  return total;
 
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  for(let i = 1; i <= a; i++){
+    total *= i;
+  }
+	return total;
 };
 
 // Do not edit below this line
