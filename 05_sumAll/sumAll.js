58e1ced643546c55e8fab7d5940be015ee8cd45f
Jason Scott <jcpoems@gmail.com>
2024-06-17 13:04:24 -0400

    Solve problem 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0 || typeof a != "number" || typeof b != "number") return "ERROR";
+    if(a > b){
+        let c = a;
+        a = b;
+        b = c;
+    }
+    let ans = 0;
+    for(let i = a; i <= b; i++){
+        ans += i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
