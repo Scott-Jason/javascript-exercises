8bf8d80d4093819099ba1e2c81856656eeaaed2c
Jason Scott <jcpoems@gmail.com>
2024-06-17 12:44:08 -0400

    Solve problem 04
    Can use array.includes("item") to see if an array has an item inside item
    can also use array.forEach((element) => something you want done on each element)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let retArr = [];
+    let lock = true;
+    for( const val of array){
+        for(const badToken of toRemove){
+            if(val == badToken && typeof val == typeof badToken) lock = false;
+        }
+        if(lock) retArr.push(val);
+        lock = true;
+    }
+    return retArr;
 };
 
 // Do not edit below this line
