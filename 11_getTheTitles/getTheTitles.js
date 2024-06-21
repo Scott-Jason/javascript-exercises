9007e367afcdb6754e8c43dbc04fd760ce125e89
Jason Scott <jcpoems@gmail.com>
2024-06-20 23:04:11 -0400

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const bookNames = [];
+    books.map(obj => bookNames.push(obj.title));
+    return bookNames;
 };
 
 // Do not edit below this line
