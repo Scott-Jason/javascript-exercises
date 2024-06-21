e436040ae00ffd1921350305e15844455df2ed91
Jason Scott <jcpoems@gmail.com>
2024-06-20 22:56:48 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = parseInt(index);
+    if( index < 0){ return "OOPS"};
+    if(index == 0) return 0;
+    
+    let a = 0;
+    let b = 1;
+    let c = 0;
+    for( let i = 0; i < index-1; i++){
+        c = b;
+        b = a+b;
+        a = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
