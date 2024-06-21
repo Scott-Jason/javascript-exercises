39b1ba2045ffadc95a7dbd03183845372b48df34
Jason Scott <jcpoems@gmail.com>
2024-06-20 22:42:33 -0400

    Complete js exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (arr) {
+    arr = arr.toLowerCase();
+    const newArr = arr.split("").filter(char => (char >= 'a' && char <= 'z')||(char >= '0' && char <= '9'));
+    const comp = newArr.join("");
+    const rev = newArr.reverse().join("");
+    console.log(rev);
+    return rev == comp ? true : false;
 };
 
 // Do not edit below this line
