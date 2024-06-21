d36fe276f3ed0a2bec05386d56aefa505b995f84
Jason Scott <jcpoems@gmail.com>
2024-06-20 23:19:30 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peeps) {
+    const d = new Date();
+    let year = d.getFullYear();
+    
+    peeps.sort((a,b) => { 
+        if(a.yearOfDeath == null) a.yearOfDeath = year;
+        if(b.yearOfDeath == null) b.yearOfDeath = year;
+        return (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) ? -1 : 1;
+    });
+    return peeps[0];
 };
 
 // Do not edit below this line
